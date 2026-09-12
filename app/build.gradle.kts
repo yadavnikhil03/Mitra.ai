c78f5d7c5ae5d443a17d8cc6e8f3bb186d0f6d94
yadavnikhil03 <nikhil181245@gmail.com>
2026-09-12 21:47:00 +0530

    chore(release): bump version to 1.0.1-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mitra.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1-beta"
         vectorDrawables.useSupportLibrary = true
         multiDexEnabled = false
     }
